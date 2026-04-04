b9c1b64d891daf37b21fa47e2b7757f12e2510af
Tom Ritchford <tom@swirly.com>
2026-04-04 15:03:52 +0200

    Add Pin, Fade and EditPoint

--- fmix/fmix.py
+++ fmix/fmix.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import dataclasses as dc
 import os
+from enum import StrEnum, auto
 
 
 @dc.dataclass(frozen=True)
@@ -18,3 +19,25 @@ class Out:
 
     def __call__(self, *files: str) -> str:
         return self.file_name or self.file_root + os.path.commonprefix(files)
+
+
+class Pin(StrEnum):
+    """Where the time at an edit point is attached to in the fade"""
+
+    begin = auto()
+    middle = auto()
+    end = auto()
+
+
+@dc.dataclass(frozen=True)
+class Fade:
+    shape: str = "linear"
+    duration: float = 1.0
+    pin: Pin = Pin.middle
+
+
+@dc.dataclass(frozen=True)
+class EditPoint:
+    time: float
+    mix: dict[str, float] = dc.field(default_factory=dict)
+    fade: Fade | None = None
