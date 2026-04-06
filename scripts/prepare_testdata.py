722fa8e8c136c3cc15e471253169fc21b568d2eb
Tom Ritchford <tom@swirly.com>
2026-04-06 15:35:31 +0200

    Add some test data

---
+++ scripts/prepare_testdata.py
@@ -0,0 +1,26 @@
+import ffmpeg
+import sys
+
+
+def loop(s):
+    (
+        ffmpeg
+        .input(s)
+        .filter('aloop', loop=3, size=10*48000)
+        .output(s.replace('m4a', 'wav'))
+        .run(cmd=['ffmpeg', '-hide_banner', '-y'])
+    )
+
+
+def run(s):
+    (
+        ffmpeg
+        .input(s)
+        .filter('atrim', end=6.3)
+        .output(s.replace('wav', 'm4a'))
+        .run(cmd=['ffmpeg', '-hide_banner', '-y'])
+    )
+
+
+for name in sys.argv[1:]:
+    run(name)
