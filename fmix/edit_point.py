c4ed3d50e726aec743504daa6342e4698432fe38
Tom Ritchford <tom@swirly.com>
2026-04-21 17:52:10 +0200

    Extract fade.py from edit_point.py

--- fmix/edit_point.py
+++ fmix/edit_point.py
@@ -4,32 +4,9 @@ import dataclasses as dc
 from collections.abc import Iterator, Sequence
 from functools import cached_property
 
-import ffmpeg as ff
-from ffmpeg.nodes import InputNode
-
 from . import time
-from .curve import Curve
 from .excepter import Excepter
-
-
-@dc.dataclass(frozen=True)
-class Fade:
-    curve: Curve = Curve.tri
-    duration: float = 1.0  # Negative means a gap!
-
-    def crossfade(self, a: InputNode, b: InputNode) -> InputNode:
-        c, d = self.curve, self.duration
-        return ff.filter((a, b), 'acrossfade', curve1=c, curve2=c, duration=d)
-
-    def fade(self, a: InputNode, type_: str) -> InputNode:
-        return ff.filter(a, 'afade', type=type_, duration=self.duration)
-
-    def check(self) -> None:
-        with Excepter('Fade') as ex:
-            if self.curve == 'linear':
-                self.__dict__['curve'] = Curve.tri
-            else:
-                ex.call(Curve, self.curve)
+from .fade import Fade
 
 
 @dc.dataclass(frozen=True)
---
+++ fmix/fade.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import dataclasses as dc
+
+import ffmpeg as ff
+from ffmpeg.nodes import InputNode
+
+from fmix.curve import Curve
+from fmix.excepter import Excepter
+
+
+@dc.dataclass(frozen=True)
+class Fade:
+    curve: Curve = Curve.tri
+    duration: float = 1.0  # Negative means a gap!
+
+    def crossfade(self, a: InputNode, b: InputNode) -> InputNode:
+        c, d = self.curve, self.duration
+        return ff.filter((a, b), 'acrossfade', curve1=c, curve2=c, duration=d)
+
+    def fade(self, a: InputNode, type_: str) -> InputNode:
+        return ff.filter(a, 'afade', type=type_, duration=self.duration)
+
+    def check(self) -> None:
+        with Excepter('Fade') as ex:
+            if self.curve == 'linear':
+                self.__dict__['curve'] = Curve.tri
+            else:
+                ex.call(Curve, self.curve)
