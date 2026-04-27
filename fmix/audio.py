3d5a59a828e27b87217537826c8a474b7c839423
Tom Ritchford <tom@swirly.com>
2026-04-27 13:49:39 +0200

    Better range checking for audio

--- fmix/audio.py
+++ fmix/audio.py
@@ -1,12 +1,29 @@
 from __future__ import annotations
 
-from pydantic import BaseModel
+import datetime as dt
+from typing import Annotated
+
+from pydantic import AfterValidator, BaseModel
+
+
+def non_negative[T](x: T) -> T:
+    if x is None:
+        return x
+    if isinstance(x, (int, float)):
+        seconds = x
+    elif isinstance(x, dt.timedelta):
+        seconds = x.total_seconds()
+    else:
+        raise TypeError(f'{x=} {type(x)=}')
+    if seconds < 0:
+        raise ValueError(f'{x} is negative')
+    return x
 
 
 class Audio(BaseModel, frozen=True):
-    start: float | None = None
-    end: float | None = None
-    gain: float = 1.0
+    start: Annotated[dt.timedelta | None, AfterValidator(non_negative)] = None
+    end: Annotated[dt.timedelta | None, AfterValidator(non_negative)] = None
+    gain: Annotated[float, AfterValidator(non_negative)] = 1.0
     normalize: bool = True
     fade_in: bool = True  # Not in use
     fade_out: bool = True  # Not in use
