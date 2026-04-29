9c7ee2b70851251c64dc8879a7bba824b02a5d2d
Tom Ritchford <tom@swirly.com>
2026-04-29 13:58:23 +0200

    Start implementing audio playback (see #6)

--- fmix/files.py
+++ fmix/files.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import os
 from pathlib import Path
 from typing import Self
 
@@ -9,12 +8,12 @@ from pydantic import BaseModel, Field, FilePath, model_validator
 
 class Files(BaseModel, frozen=True):
     inputs: dict[str, FilePath] = Field(default_factory=dict)
-    output: Path = Path('fmix-output.wav')
+    output: Path | None = None
     overwrite: bool = True
 
     @model_validator(mode='after')
     def check_overwrite(self) -> Self:
-        if os.path.exists(self.output):
+        if self.output and self.output.exists():
             if not self.overwrite:
                 raise FileExistsError(f'{self.output=} overwrites an existing file')
             if any(self.output.samefile(i) for i in self.inputs.values()):
--- fmix/fmix.py
+++ fmix/fmix.py
@@ -1,17 +1,21 @@
 from __future__ import annotations
 
 import datetime as dt
+import sys
 from collections.abc import Sequence
 from functools import cached_property
 
 import numpy as np
 from pydantic import BaseModel
+from tuney.audio.players import DataPlayer
+from tuney.audio.sample_data import SampleData
 
 from . import audio_file
 from .audio import INF, Audio, SampleRate
 from .edit_point import EditPoint
 from .fade import Fade
 from .files import Files
+from .render import render_samples
 
 
 class FMix(BaseModel, frozen=True):
@@ -19,6 +23,7 @@ class FMix(BaseModel, frozen=True):
     edit_point: Sequence[EditPoint] = ()
     fade: Fade = Fade()
     files: Files = Files()
+    sound: bool | None = None
 
     @cached_property
     def edit_points(self) -> list[EditPoint]:
@@ -48,3 +53,14 @@ class FMix(BaseModel, frozen=True):
     def sample_ends(self) -> list[int]:
         m = max(s.shape[0] for s, _ in self.data_and_rates.values())
         return [min(m, self.samplerate(e.seconds)) for e in self.edit_points]
+
+    def __call__(self) -> None:
+        result = self.audio(render_samples(self), self.samplerate)
+        if self.files.output is None and not self.sound:
+            print('Nothing to do', file=sys.stderr)
+            return
+        if self.files.output is not None:
+            audio_file.write(self.files.output, result, self.samplerate)
+        if self.sound or self.files.output is None:
+            player = DataPlayer(SampleData(result, self.samplerate))
+            player.run()
