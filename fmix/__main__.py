f35d2ad8118fae6a198dee5933466c1d748a277c
Tom Ritchford <tom@swirly.com>
2026-05-03 14:57:39 +0200

    Add a simple `tyro` CLI

--- fmix/__main__.py
+++ fmix/__main__.py
@@ -1,5 +1,19 @@
+from pathlib import Path
+
+import tyro
+
+from . import fmix
+
+
+class FMix(fmix.FMix, frozen=True):
+    config_file: Path | None = None
+
+
 def main():
-    print('Hello from fmix!')
+    from .audio import Audio
+
+    args = tyro.cli(Audio)
+    print(args)
 
 
 if __name__ == '__main__':
